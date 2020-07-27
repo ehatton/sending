c409df968c4b21f9c08e81005ab8eb41bdb3b8ca
Emma Hatton-Ellis <ehattonellis@gmail.com>
2020-07-27 11:18:54 +0100

    Updating build scripts

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@ from setuptools import setup
 
 setup(
     name="sending",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/ehatton/sending",
     author="Emma Hatton-Ellis",
     author_email="ehattonellis@gmail.com",
     license="MIT",
     py_modules=["sending"],
-    install_requires=["click"],
-    python_requires=">=3.8",
-    entry_points="""
-        [console_scripts]
-        sending=sending.cli:cli
-    """,
+    install_requires=["click", "pysftp", "requests"],
+    python_requires=">=3.7",
+    entry_points={"console_scripts": ["sending=sending.cli:cli",],},
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Utilities",
+    ],
 )
+
